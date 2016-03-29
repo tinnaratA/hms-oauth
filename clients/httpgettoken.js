d6801713678a671250da9b0e5cce9aa047ff0a13
TinnaratA <tinnarat.ar@glsict.com>
2016-03-29 11:14:00 +0700

    Renew client id and client secret

--- clients/httpgettoken.js
+++ clients/httpgettoken.js
@@ -1,24 +1,24 @@
 var http = require('http');
- 
+
 // Set the headers
 var headers = {
     'User-Agent':       'Super Agent/0.0.1',
     'Content-Type':     'application/x-www-form-urlencoded'
 }
- 
+
 // Configure the request
 //var uri = 'http://10.105.163.62:8000/o/token/?grant_type=password&username=admin&password=admin@1234&client_id=odQEaUKYZy9dinS1vhvZj6xotgbj5YEg9vFDDggN&client_secret=n7VAmhVOtF9CEBgrK3DHMtoZitcZgMPQW2lvXuCthyarucjKqP1ln5gzsHNLjga8YDCRWfiZk5KyFncYdRhhwzDDZljswd8O02NStrto0r78RoK5Smmj3uAHpkejs29R'
 var options = {
     method: 'POST',
     host: '127.0.0.1',
     port: '8000',
-    path: '/o/token/?grant_type=password&username=admin&password=admin@1234&client_id=oNqcvFrINsdc5l2vJxkYRKtBBg6TWr0ktJN0tSBV&client_secret=Bhc5wl09ih34MC8EEoJzrFukBNayZ7egQP4Zyzi6xtFqtGzXqFhQPBXXxRRkfMZ7swUB6Tf4f3rm2G6zJFXrrMP8OHoeXHKYsmgbIHAZa8YeVvkdm1mVJdeark7XwTMI'
+    path: '/o/token/?grant_type=password&username=admin&password=admin@1234&client_id=7FK6YzhqE94whFFSsssZM0GkJsqUuCmGrvShCjyA&client_secret=7dFBEKlff5wn8QWWulYBh8Plvhtk5aTKDnHDrrGizgjM1MT4sxysCsnijJUyvanCSoUsi8T5FtR2LGEOC3xC6tAxwlX5aJonhT0s84PlHQeeQdSs2ndDysyCTAh1Nfse'
     }
 // Start the request
 var request =  http.request(options, function (res) {
 //var request = http.request(uri, function(res) {
   var chunks = [];
-   
+
   res.on("data", function (chunk) {
     chunks.push(chunk);
     //console.log("response.dsta="+)
@@ -49,7 +49,7 @@ var request =  http.request(options, function (res) {
         users_http_request.auth = 'Bearer '+access_token;
        // console.log('request url:'+users_http_request.url);
         var user_chunks =[];
-        
+
         res.on("data", function(chunk){
             user_chunks.push(chunk);
             //console.log("users_http_request.ondata.chunk"+chunk);
@@ -58,7 +58,7 @@ var request =  http.request(options, function (res) {
 
         res.on("end", function(){
           var body = Buffer.concat(user_chunks);
-          console.log("users_http_request.end");
+          console.log("users_http_request.end"+body);
         });
       //
     });
@@ -68,10 +68,10 @@ var request =  http.request(options, function (res) {
       //TODO:
     });
     users_http_request.end();
-   
+
   /**  var users_request = http.request(userapi_options, function (res) {
         var chunks = [];
-   
+
   res.on("data", function (chunk) {
     chunks.push(chunk);
     //console.log("response.dsta="+)
